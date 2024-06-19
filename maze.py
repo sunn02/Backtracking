6655c210533a02c055857c33ac099cbb4ca00e74
Sol <sollopez0002@hotmail.com>
2024-06-18 22:26:04 -0400

    Initial commit

---
+++ maze.py
@@ -0,0 +1,10 @@
+#0 for blocked cell
+#1 for free cell
+#order N*N
+#source at 0,0 and the destinations is N-1, N-1
+#movements are up, down, left, right
+
+
+def find_Path(m,n): #(m is matrix, n for the size)
+    
+    
