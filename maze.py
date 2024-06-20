facc178982e5d8036b471bd0637d1b7f8ea693bd
Sol <sollopez0002@hotmail.com>
2024-06-19 20:50:38 -0400

    Almost

--- maze.py
+++ maze.py
@@ -8,41 +8,50 @@ import random
 
 directions = ((1,0,'D'),(-1,0,'U'),(0,1,'R'),(0,-1,'L'))
 
-def isValid(i,j,m): #si esta dentro de los limites de la matriz
-    return 0<=i < len(m) and 0<=j<len(m[0]) 
+def isValid(i,j,maze): #si esta dentro de los limites de la matriz
+    return 0<=i < len(maze) and 0<=j<len(maze[0]) 
 
-def dfs(i,j,m,visited,ans,path): 
-    if i == len(m)-1 and j == len(m[0])-1: #if we reach th destination
+def dfs(i,j,maze,visited,ans,path): 
+    if i == len(maze)-1 and j == len(maze[0])-1: #if we reach th destination
         ans.append(path)
         return 
     visited.add((i,j))  #if not 
 
     for k in directions:
         newi, newj = i+k[0], j+k[1]
-        if isValid(newi, newj, m) and (newi, newj) not in visited and m[newi][newj] == 1:
-            dfs(newi,newj,m,visited,ans,path+k[2])
+        if isValid(newi, newj, maze) and (newi, newj) not in visited and maze[newi][newj] == 1:
+            dfs(newi,newj,maze,visited,ans,path+k[2])
     visited.remove((i,j))
 
 
-def find_Path(m,n): #(m is matrix, n for the size) initial call
+def find_Path(maze,n): #(m is matrix, n for the size) initial call
     ans = []
-    if m[0][0] == 0 or m[n-1][n-1] == 0:
+    if maze[0][0] == 0 or maze[n-1][n-1] == 0:
         return ans
     path = ""
     visited = set() 
-    dfs(0,0,m,visited,ans,path)
+    dfs(0,0,maze,visited,ans,path)
     return ans
 
 def generate_maze(n):
+    maze = [[1 for _ in range(n)] for _ in range(n)] 
     
+    for i in range(n):
+        for j in range(n):
+            if(i,j) != (0,0) and (i,j) != (n-1,n-1):
+                if random.choice([0,1]) == 0:
+                    maze[i][j] = 0
+    return maze
 
+n = 10
+maze = generate_maze(n)
+for row in maze:
+    print(row)
 
-m = [
-    [1, 0, 0, 0],
-    [1, 1, 0, 1],
-    [0, 1, 0, 0],
-    [1, 1, 1, 1]
-]
-n = 4
-paths = find_Path(m, n)
-print(paths) 
+# Encontrar todos los caminos posibles en el laberinto generado
+paths = find_Path(maze, n)
+
+# Imprimir los caminos encontrados
+print("\nPaths Found:")
+for path in paths:
+    print(path)
